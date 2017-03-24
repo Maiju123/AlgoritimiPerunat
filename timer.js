ac6058f6c26782f2eb31ae7deafcb43125804613
K2421 <viivi_2@hotmail.com>
2017-03-24 12:53:46 +0200

    Add files via upload

--- timer.js
+++ timer.js
@@ -1,32 +1,22 @@
-   function timer() {
+  // function timer() {
   
+    //   var nappi = document.getElementById("nappi");
+       
+   // if (nappi.clicked == true) {
+    $("#nappi").click(function() {
+    
         var start = new Date().getTime();   
         var stop = new Date().getTime();
         var elapsed = stop - start;
-       var nappi = document.getElementById("nappi");
-       
-    if (nappi.clicked == true) {
   
-  document.getElementById("demo").innerHTML = elapsed;
+ // document.getElementById("demo").innerHTML = elapsed;
 
+        $("#aika").append(elapsed + " millisekuntia");
 
-    };
-};
-
-
-
- //function Choice () {
-   // var box = document.getElementById("box");
-   // var yes = document.getElementById("yes");
-   // var no = document.getElementById("no");
-
-   //if (yes.clicked == true) {
-     //   box.style.backgroundColor = "red";
-   // } else if (no.clicked == true) {
-     //   box.style.backgroundColor = "green";
-   // } else {
-    //    box.style.backgroundColor = "purple";
   //  };
 //};
+});
+//timer();
 
-//Choice ();
+
+
